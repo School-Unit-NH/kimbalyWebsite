4acc76f5be37079ae33d766a2f31a5e7ec4cd948
phanstudio <donenoelle@gmail.com>
2026-02-24 13:30:03 +0100

    update

--- js/team-data.js
+++ js/team-data.js
@@ -12,133 +12,133 @@ window.teamData = {
         "name":  "Odubanjo Christiana",
         "role":  "Non teaching staff",
         "courses":  "Non teaching staff",
-        "image":  "team-01.webp"
+        "image":  "IMG_5698.JPEG"
     },
     {
         "id":  "teacher-3",
         "name":  "Ibrahim",
         "role":  "Grade 3",
         "courses":  "Primary school subject",
-        "image":  "team-01.webp"
+        "image":  "IMG_5699.JPEG"
     },
     {
         "id":  "teacher-4",
         "name":  "Oluwaleimu Adebola",
         "role":  "Grade 4",
         "courses":  "ALL",
-        "image":  "team-01.webp"
+        "image":  "IMG_5700.JPEG"
     },
     {
         "id":  "teacher-5",
         "name":  "Anthony Success",
         "role":  "Assistant teacher nur 1",
         "courses":  "Assistant teacher",
-        "image":  "team-01.webp"
+        "image":  "IMG_5701.PNG"
     },
     {
         "id":  "teacher-6",
         "name":  "Okaiyeto Abiodun Alexander",
         "role":  "Grade 6",
         "courses":  "Mathematics, English language",
-        "image":  "team-01.webp"
+        "image":  "IMG_5702.JPEG"
     },
     {
         "id":  "teacher-7",
         "name":  "Miss Roselyn Sani",
         "role":  "Nursery two",
         "courses":  "Nursery section",
-        "image":  "team-01.webp"
+        "image":  "IMG_5703.JPEG"
     },
     {
         "id":  "teacher-8",
         "name":  "Aladejare Tolulope Elizabeth",
         "role":  "Grade 5",
         "courses":  "All subjects",
-        "image":  "team-01.webp"
+        "image":  "IMG_5704.JPEG"
     },
     {
         "id":  "teacher-9",
         "name":  "Mrs Richard",
         "role":  "Nur I",
         "courses":  "Numeracy,literacy,psed and kyw",
-        "image":  "team-01.webp"
+        "image":  "IMG_5705.JPEG"
     },
     {
         "id":  "teacher-10",
         "name":  "Jacob Martha Omoh",
         "role":  "Year one",
         "courses":  "All subjects",
-        "image":  "team-01.webp"
+        "image":  "IMG_5706.JPEG"
     },
     {
         "id":  "teacher-11",
         "name":  "Tega Tessy Johnson",
         "role":  "Preschool",
         "courses":  "Non",
-        "image":  "team-01.webp"
+        "image":  "IMG_5707.JPEG"
     },
     {
         "id":  "teacher-12",
         "name":  "Dada Oluwasegun",
         "role":  "Non teaching staff",
         "courses":  "Non teaching staff",
-        "image":  "team-01.webp"
+        "image":  "IMG_5708.JPEG"
     },
     {
         "id":  "teacher-13",
         "name":  "Musa Francis",
         "role":  "Non teaching staff",
         "courses":  "Non teaching staff",
-        "image":  "team-01.webp"
+        "image":  "IMG_5709.JPEG"
     },
     {
         "id":  "teacher-14",
         "name":  "Eno Bassey",
         "role":  "Non teaching staff",
         "courses":  "Non teaching staff",
-        "image":  "team-01.webp"
+        "image":  "IMG_5710.JPEG"
     },
     {
         "id":  "teacher-15",
         "name":  "Musa Bukola",
         "role":  "Non teaching staff",
         "courses":  "Non teaching staff",
-        "image":  "team-01.webp"
+        "image":  "IMG_5711.JPEG"
     },
     {
         "id":  "teacher-16",
         "name":  "Sandra Odufuwa",
         "role":  "Year 2",
         "courses":  "Lower primary",
-        "image":  "team-01.webp"
+        "image":  "IMG_5712.JPEG"
     },
     {
         "id":  "teacher-17",
         "name":  "Olanlege Sunday olaitan",
         "role":  "Basic three (3)",
         "courses":  "Elementary subjects",
-        "image":  "team-01.webp"
+        "image":  "IMG_5713.JPEG"
     },
     {
         "id":  "teacher-18",
         "name":  "Ehimuan Blessing",
         "role":  "Year 2",
         "courses":  "All subjects",
-        "image":  "team-01.webp"
+        "image":  "IMG_5714.JPEG"
     },
     {
         "id":  "teacher-19",
         "name":  "Comfort oyak",
         "role":  "Nursery 2",
         "courses":  "All subjects in nursery 2 class",
-        "image":  "team-01.webp"
+        "image":  "IMG_5715.JPEG"
     },
     {
         "id":  "teacher-20",
         "name":  "Yusuff oluwatoyin",
         "role":  "Preschool Tiw",
         "courses":  "Nursery class",
-        "image":  "team-01.webp"
+        "image":  "IMG_5716.JPEG"
     }
 ]
 };
